729b75974f01aaebd2f885a41c24cc01f4d212aa
Abhiram P Ashok <93904779+Abhiram-P-Ashok@users.noreply.github.com>
2024-01-30 13:59:44 +0530

    pushing model folder

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,33 @@
+var model;
+var img;
+var classNames = [];
+var _validFileExtensions = [".jpg", ".jpeg", ".png"];    
+function Validate(oForm) {
+    img = oForm.getElementsByTagName("input");
+    for (var i = 0; i < img.length; i++) {
+        var oInput = img[i];
+        if (oInput.type == "file") {
+            var sFileName = oInput.value;
+            if (sFileName.length > 0) {
+                var blnValid = false;
+                for (var j = 0; j < _validFileExtensions.length; j++) {
+                    var sCurExtension = _validFileExtensions[j];
+                    if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+                        blnValid = true;
+                        break;
+                    }
+                }
+                
+                if (!blnValid) {
+                    alert("Sorry, " + sFileName + " is invalid, allowed extensions are: " + _validFileExtensions.join(", "));
+                    return false;
+                }
+            }
+        }
+    }
+  
+    return true;
+}
 function uploadAndPredict() {
     var formData = new FormData($('#uploadForm')[0]);
 
